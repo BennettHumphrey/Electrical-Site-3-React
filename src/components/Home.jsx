2b67f915d0c3b1af58827b9ada806e67c39ed2c1
Ben Humphrey <bennetthumphrey98@gmail.com>
2024-05-18 13:08:23 -0500

    Adjust DesktopNav, prep for vercel deployment

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,10 +5,15 @@ import Header from './Header'
 import Nav from './Nav/Nav'
 import Reviews from './Reviews'
 import WhatWeDo from './WhatWeDo'
+import Button from './Button'
 
 const Home = () => {
 
- 
+ const data = [
+  {href:"href1", text:"text1"},
+   {href:"href2", text:"text2"},
+   {href:"href3", text:"text3"}
+]
 
   
   return (
@@ -19,6 +24,10 @@ const Home = () => {
         <AboutUs  />
         <Reviews  />
         <Footer  />
+        {/* {data.map((arrayIndex, i) => (
+        <Button key={i} href={arrayIndex.href} text={arrayIndex.text} /> */}
+
+       
     </>
   )
 }
--- src/components/Nav/DesktopNav.jsx
+++ src/components/Nav/DesktopNav.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { navOptions } from '../../resources/data'
 import { Link } from 'react-router-dom'
 
+
+
 const DesktopNav = () => {
   return (
     <div className='sticky bg-main z-10 flex justify-evenly h-12 text-xl' >
@@ -15,7 +17,7 @@ const DesktopNav = () => {
         </Link>
         {navOptions.map((o, i) => (
                     <Link key={i} className='h-full flex items-center px-6 duration-500 hover:bg-menu hover:text-text-light active:bg-menu active:text-text-light active:duration-0' 
-                                to={o.href}>
+                                to="/Electrical-Site-3-React/">
                         <p>{o.title}</p>
                     </Link>
                 ))}
